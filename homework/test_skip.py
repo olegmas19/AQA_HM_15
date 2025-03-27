7a4f41076e8cde1b531759721025ace53bdbb9ee
Oleg Maslennikov <olegmas19@mail.ru>
2025-03-27 21:24:24 +0300

    add test_skip.py

--- homework/test_skip.py
+++ homework/test_skip.py
@@ -6,21 +6,30 @@ from selene import browser, be
 Пропустите мобильный тест, если соотношение сторон десктопное (и наоборот)
 """
 
-mobile_size_1 = (414, 896)
-mobile_size_2 = (430, 932)
-desktop_size_1 = (1280, 720)
-desktop_size_2 = (640, 360)
 
-@pytest.fixture(params=[mobile_size_1, mobile_size_2, desktop_size_1, desktop_size_2])
+@pytest.fixture(params=[(1280, 720), (1400, 600), (1920, 1080), (414, 890), (439, 932), (375, 667)])
 def browser_options_desktop(request):
-
-    pass
+    browser.open('https://github.com/')
+    width, height = request.param
+    browser.driver.set_window_size(width, height)
+    if width <= 439:
+        yield 'mobile'
+        browser.quit()
+    else:
+        yield 'desktop'
+        browser.quit()
 
 
 def test_github_desktop(browser_options_desktop):
-    browser.element('.HeaderMenu-link--sign-up').should(be.visible).click()
+    if browser_options_desktop == 'mobile':
+        pytest.skip(reason='Разрешение браузера для моб.устройств')
+    else:
+        browser.element('.HeaderMenu-link--sign-up').should(be.visible).click()
 
 
 def test_github_mobile(browser_options_desktop):
-    browser.element('.Button-content').should(be.visible).click()
-    browser.element('.HeaderMenu-link--sign-up').should(be.visible).click()
+    if browser_options_desktop == 'desktop':
+        pytest.skip(reason='Разрешение браузера десктопное')
+    else:
+        browser.element('.Button-content').should(be.visible).click()
+        browser.element('.HeaderMenu-link--sign-up').should(be.visible).click()
